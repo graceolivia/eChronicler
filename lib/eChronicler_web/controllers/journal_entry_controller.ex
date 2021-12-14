74d8036282ebe26bda768040f725b9c839d75671
Grace <gracelidinskysmith@gmail.com>
2021-12-14 16:31:34 -0500

    Add HTML page with entries

--- lib/eChronicler_web/controllers/journal_entry_controller.ex
+++ lib/eChronicler_web/controllers/journal_entry_controller.ex
@@ -2,7 +2,11 @@ defmodule EChroniclerWeb.JournalEntryController do
   use EChroniclerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    journal_entries = [%EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test."},
+    %EChronicler.EChronicler.Journal_Entry{title: "Cool Blog Post", author: "Grace", entry: "Test."},
+    %EChronicler.EChronicler.Journal_Entry{title: "So-So Blog Post", author: "Grace", entry: "Test."}]
+
+    render(conn, "index.html", journal_entries: journal_entries)
   end
 
 end
