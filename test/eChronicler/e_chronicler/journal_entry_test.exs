917b00bd892138f7b62281cd2eaddb7009472974
Grace <gracelidinskysmith@gmail.com>
2021-12-15 16:54:54 -0500

    Fix typo

--- test/eChronicler/e_chronicler/journal_entry_test.exs
+++ test/eChronicler/e_chronicler/journal_entry_test.exs
@@ -6,12 +6,12 @@ defmodule EChronicler.EChronicler.JournalEntry  do
   alias EChronicler.JournalEntries.JournalEntry
 
   test "format_datetime/1 returns formatted string if correct" do
-    {:ok, correct_datetime} = %EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: "2021-12-14 21:51:06"}
+    {:ok, correct_datetime} = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: "2021-12-14 21:51:06"}
     assert correct_datetime.format_datetime() == :ok
   end
 
   test "format_datetime/1 returns error if incorrect" do
-    {:ok, incorrect_datetime} = %EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
+    {:ok, incorrect_datetime} = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
     assert incorrect_datetime.format_datetime() == :error
   end
 
