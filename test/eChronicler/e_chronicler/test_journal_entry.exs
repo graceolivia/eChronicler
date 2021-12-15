bd05af42b96d46e9f3050781981a6dcc3792dd17
Grace <gracelidinskysmith@gmail.com>
2021-12-15 16:33:19 -0500

    Fix mistake in test

--- test/eChronicler/e_chronicler/test_journal_entry.exs
+++ test/eChronicler/e_chronicler/test_journal_entry.exs
@@ -7,12 +7,12 @@ defmodule EChronicler.EChronicler.JournalEntry  do
 
   test "format_datetime/1 returns formatted string if correct" do
     {:ok, correct_datetime} = %EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: "2021-12-14 21:51:06"}
-    assert %EChronicler.EChronicler.Journal_Entry.format_datetime() == :ok
+    assert correct_datetime.format_datetime() == :ok
   end
 
   test "format_datetime/1 returns error if incorrect" do
-    {:ok, correct_datetime} = %EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
-    assert %EChronicler.EChronicler.Journal_Entry.format_datetime() == :error
+    {:ok, incorrect_datetime} = %EChronicler.EChronicler.Journal_Entry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
+    assert incorrect_datetime.format_datetime() == :error
   end
 
 end
