cfb0acba98f015df337180da81200f03ab06d68b
Grace <gracelidinskysmith@gmail.com>
2021-12-16 10:45:52 -0500

    Add tests that work

--- test/eChronicler/e_chronicler/journal_entry_test.exs
+++ test/eChronicler/e_chronicler/journal_entry_test.exs
@@ -1,18 +1,19 @@
-defmodule EChronicler.EChronicler.JournalEntry  do
+defmodule EChronicler.EChronicler.JournalEntryTest  do
   use EChronicler.DataCase, async: true
   import Ecto.Query, warn: false
 
   alias EChronicler.EChronicler.JournalEntries
-  alias EChronicler.EChronicler.JournalEntries.JournalEntry
+  alias EChronicler.EChronicler.JournalEntry
 
   test "format_datetime/1 returns formatted string if correct" do
-    {:ok, correct_datetime} = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: "2021-12-14 21:51:06"}
-    assert correct_datetime.format_datetime() == :ok
+    correct_datetime = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: "2021-12-14 10:51:06"}
+    IO.inspect(correct_datetime)
+    assert JournalEntry.format_datetime(correct_datetime) == "10:51, December 14, 2021"
   end
 
   test "format_datetime/1 returns error if incorrect" do
-    {:ok, incorrect_datetime} = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
-    assert incorrect_datetime.format_datetime() == :error
+    incorrect_datetime = %EChronicler.EChronicler.JournalEntry{title: "Awesome Blog Post", author: "Grace", entry: "Test.", inserted_at: ""}
+    assert JournalEntry.format_datetime(incorrect_datetime)  == "no date found"
   end
 
 end
