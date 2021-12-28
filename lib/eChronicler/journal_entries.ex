f00d48d812fde36f33077a15dbf86f258fdfd3e8
Grace <gracelidinskysmith@gmail.com>
2021-12-28 15:17:51 -0500

    Change e_chronicler directory name to models

--- lib/eChronicler/journal_entries.ex
+++ lib/eChronicler/journal_entries.ex
@@ -2,12 +2,11 @@ defmodule EChronicler.JournalEntries do
   import Ecto.Query, warn: false
 
   alias EChronicler.Repo
-  alias EChronicler.EChronicler.JournalEntry
+  alias EChronicler.Models.JournalEntry
 
   def list_journal_entries() do
     Repo.all(JournalEntry)
     |> Enum.sort(&(&1.inserted_at > &2.inserted_at))
   end
-
 
 end
